b4f1460c79501c1e50c5ba48b3aed1a2abfbdebd
Jesse Bounds <jesse@rebounds.net>
2012-08-31 16:38:33 -0700

    added post hook for token registration and save to mongo db

--- pusher.js
+++ pusher.js
@@ -14,15 +14,37 @@ var options = {
     cacheLength: 100                  /* Number of notifications to cache for error purposes */
 };
 
+// push
 var apnsConnection = new apns.Connection(options);
 var myDevice = new apns.Device("daa91a4c 12c00203 c8adbdd2 9a1445fb bb4d77a9 88108b08 807f78f0 2e462942");
 
+//mongo
+var mongo = require('mongodb');
+var url = require('url');
+var connectionUri = url.parse(process.env.MONGOHQ_URL);
+var dbName = connectionUri.pathname.replace(/^\//, '');
+    
+// express
 var express = require('express');
 var app = express();
 app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
   res.send('hello world');
+});
+app.post('/register', function(req, res) {
+  var token = req.param("token");
+  console.log(token);
+  mongo.Db.connect(process.env.MONGOHQ_URL, function(error, client) {
+    if (error) throw error;
+
+    client.collection('test', function(err, collection) {
+      var doc = {token: token};
+      collection.insert(doc);
+    });
+  });  
+
+  res.status(201).send('');  
 });
 app.post('/github/webhook', function(req, res) {
   var payload = JSON.parse(req.param("payload", null));
