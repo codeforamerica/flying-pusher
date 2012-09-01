92459994b1201fb18d617d7025deef8cf3bee2bb
Jesse Bounds <jesse@rebounds.net>
2012-08-31 17:27:45 -0700

    changing register to GET for now

--- pusher.js
+++ pusher.js
@@ -32,7 +32,7 @@ app.use(express.bodyParser());
 app.get('/', function(req, res) {
   res.send('hello world');
 });
-app.post('/register', function(req, res) {
+app.get('/register', function(req, res) {
   var token = req.param("token");
   console.log(token);
   mongo.Db.connect(process.env.MONGOHQ_URL, function(error, client) {
@@ -44,7 +44,7 @@ app.post('/register', function(req, res) {
     });
   });  
 
-  res.status(201).send('');  
+  res.status(200).send('');  
 });
 app.post('/github/webhook', function(req, res) {
   var payload = JSON.parse(req.param("payload", null));
